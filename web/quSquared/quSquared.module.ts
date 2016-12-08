e34784f7f8f3fb3457b7c490e6f6ecbcdc12f3d9
michael.taylor@qusquared.com <miketaylor@rgare.com>
2016-12-08 14:05:24 -0600

    Fixed routing to not cause page reload

--- web/quSquared/quSquared.module.ts
+++ web/quSquared/quSquared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { quSquaredProvider } from './quSquared.provider';
 
 @NgModule({
 	declarations: [
 		quSquaredProvider
+	],
+	imports: [
+		RouterModule
 	],
 	exports: [
 		quSquaredProvider
