6184cb839ae704d37249e367dbd79b1591bcc388
Michael Taylor <mtaylor3390@gmail.com>
2016-12-25 17:57:51 -0600

    Web: made side nav scrollable.

--- web/src/profile/profile.routes.ts
+++ web/src/profile/profile.routes.ts
@@ -8,6 +8,9 @@ import { ProfileViewComponent } from './profile-view/profile-view.comp';
 const routes: Routes = [
    { path: "profile/create", component: ProfileCreateComponent },
    { path: "profile/edit", component: ProfileEditComponent },
-   { path: "profile/:name/view", component: ProfileViewComponent }
+   { path: "profile/:name", component: ProfileViewComponent },
+   { path: "profile/:name/tricks", component: ProfileViewComponent },
+   { path: "profile/:name/rank", component: ProfileViewComponent },
+   { path: "profile/:name/games", component: ProfileViewComponent }
 ];
 export const ProfileRoutes: ModuleWithProviders = RouterModule.forChild(routes);
